26c7b1bb775671e13fb70fac66e61271b066be39
Anthony Kruger <60028700+AnthonyGKruger@users.noreply.github.com>
2020-01-18 14:40:35 +0200

    Add files via upload

---
+++ Question_2/Question 2/Question 2/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RGIT_annual_asset_depreciation_App
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblAssetCost = New System.Windows.Forms.Label()
+        Me.lblSalvageValue = New System.Windows.Forms.Label()
+        Me.lblUsefulLife = New System.Windows.Forms.Label()
+        Me.lblDepreciationSchedules = New System.Windows.Forms.Label()
+        Me.lblDdb = New System.Windows.Forms.Label()
+        Me.lblSyd = New System.Windows.Forms.Label()
+        Me.lstDdb = New System.Windows.Forms.ListBox()
+        Me.lstSyd = New System.Windows.Forms.ListBox()
+        Me.txtCost = New System.Windows.Forms.TextBox()
+        Me.txtSalvage = New System.Windows.Forms.TextBox()
+        Me.cmbUsefulLife = New System.Windows.Forms.ComboBox()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblAssetCost
+        '
+        Me.lblAssetCost.AutoSize = True
+        Me.lblAssetCost.Location = New System.Drawing.Point(80, 81)
+        Me.lblAssetCost.Name = "lblAssetCost"
+        Me.lblAssetCost.Size = New System.Drawing.Size(118, 25)
+        Me.lblAssetCost.TabIndex = 0
+        Me.lblAssetCost.Text = "Asset cost:"
+        '
+        'lblSalvageValue
+        '
+        Me.lblSalvageValue.AutoSize = True
+        Me.lblSalvageValue.Location = New System.Drawing.Point(44, 182)
+        Me.lblSalvageValue.Name = "lblSalvageValue"
+        Me.lblSalvageValue.Size = New System.Drawing.Size(154, 25)
+        Me.lblSalvageValue.TabIndex = 2
+        Me.lblSalvageValue.Text = "Salvage value:"
+        '
+        'lblUsefulLife
+        '
+        Me.lblUsefulLife.AutoSize = True
+        Me.lblUsefulLife.Location = New System.Drawing.Point(91, 288)
+        Me.lblUsefulLife.Name = "lblUsefulLife"
+        Me.lblUsefulLife.Size = New System.Drawing.Size(113, 25)
+        Me.lblUsefulLife.TabIndex = 4
+        Me.lblUsefulLife.Text = "Useful life:"
+        '
+        'lblDepreciationSchedules
+        '
+        Me.lblDepreciationSchedules.AutoSize = True
+        Me.lblDepreciationSchedules.Location = New System.Drawing.Point(563, 81)
+        Me.lblDepreciationSchedules.Name = "lblDepreciationSchedules"
+        Me.lblDepreciationSchedules.Size = New System.Drawing.Size(237, 25)
+        Me.lblDepreciationSchedules.TabIndex = 6
+        Me.lblDepreciationSchedules.Text = "Depreciation schedules"
+        '
+        'lblDdb
+        '
+        Me.lblDdb.AutoSize = True
+        Me.lblDdb.Location = New System.Drawing.Point(609, 176)
+        Me.lblDdb.Name = "lblDdb"
+        Me.lblDdb.Size = New System.Drawing.Size(272, 25)
+        Me.lblDdb.TabIndex = 7
+        Me.lblDdb.Text = "Double-declining balances:"
+        '
+        'lblSyd
+        '
+        Me.lblSyd.AutoSize = True
+        Me.lblSyd.Location = New System.Drawing.Point(974, 182)
+        Me.lblSyd.Name = "lblSyd"
+        Me.lblSyd.Size = New System.Drawing.Size(244, 25)
+        Me.lblSyd.TabIndex = 9
+        Me.lblSyd.Text = "Sum-of-the-year's digits:"
+        '
+        'lstDdb
+        '
+        Me.lstDdb.FormattingEnabled = True
+        Me.lstDdb.ItemHeight = 25
+        Me.lstDdb.Location = New System.Drawing.Point(614, 210)
+        Me.lstDdb.Name = "lstDdb"
+        Me.lstDdb.Size = New System.Drawing.Size(316, 279)
+        Me.lstDdb.TabIndex = 8
+        '
+        'lstSyd
+        '
+        Me.lstSyd.FormattingEnabled = True
+        Me.lstSyd.ItemHeight = 25
+        Me.lstSyd.Location = New System.Drawing.Point(979, 210)
+        Me.lstSyd.Name = "lstSyd"
+        Me.lstSyd.Size = New System.Drawing.Size(316, 279)
+        Me.lstSyd.TabIndex = 10
+        '
+        'txtCost
+        '
+        Me.txtCost.Location = New System.Drawing.Point(204, 75)
+        Me.txtCost.Name = "txtCost"
+        Me.txtCost.Size = New System.Drawing.Size(178, 31)
+        Me.txtCost.TabIndex = 1
+        '
+        'txtSalvage
+        '
+        Me.txtSalvage.Location = New System.Drawing.Point(204, 176)
+        Me.txtSalvage.Name = "txtSalvage"
+        Me.txtSalvage.Size = New System.Drawing.Size(178, 31)
+        Me.txtSalvage.TabIndex = 3
+        '
+        'cmbUsefulLife
+        '
+        Me.cmbUsefulLife.FormattingEnabled = True
+        Me.cmbUsefulLife.Items.AddRange(New Object() {"3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"})
+        Me.cmbUsefulLife.Location = New System.Drawing.Point(204, 280)
+        Me.cmbUsefulLife.Name = "cmbUsefulLife"
+        Me.cmbUsefulLife.Size = New System.Drawing.Size(178, 33)
+        Me.cmbUsefulLife.TabIndex = 5
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(49, 409)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(229, 80)
+        Me.btnDisplay.TabIndex = 11
+        Me.btnDisplay.Text = "&Display depreciation schedules"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(298, 409)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(135, 80)
+        Me.btnExit.TabIndex = 12
+        Me.btnExit.Text = "&Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'RGIT_annual_asset_depreciation_App
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1395, 611)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.cmbUsefulLife)
+        Me.Controls.Add(Me.txtSalvage)
+        Me.Controls.Add(Me.txtCost)
+        Me.Controls.Add(Me.lstSyd)
+        Me.Controls.Add(Me.lstDdb)
+        Me.Controls.Add(Me.lblSyd)
+        Me.Controls.Add(Me.lblDdb)
+        Me.Controls.Add(Me.lblDepreciationSchedules)
+        Me.Controls.Add(Me.lblUsefulLife)
+        Me.Controls.Add(Me.lblSalvageValue)
+        Me.Controls.Add(Me.lblAssetCost)
+        Me.Name = "RGIT_annual_asset_depreciation_App"
+        Me.Text = "RGIT annual asset depreciation App."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAssetCost As Label
+    Friend WithEvents lblSalvageValue As Label
+    Friend WithEvents lblUsefulLife As Label
+    Friend WithEvents lblDepreciationSchedules As Label
+    Friend WithEvents lblDdb As Label
+    Friend WithEvents lblSyd As Label
+    Friend WithEvents lstDdb As ListBox
+    Friend WithEvents lstSyd As ListBox
+    Friend WithEvents txtCost As TextBox
+    Friend WithEvents txtSalvage As TextBox
+    Friend WithEvents cmbUsefulLife As ComboBox
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Question_2/Question 2/Question 2/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class RGIT_annual_asset_depreciation_App
+
+    'Declaration of variables Globally'
+    Dim cost, salvageVal, usefulLife As Double
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+
+        'Clearing lstDdb and lstSyd respectivley'
+        lstDdb.Items.Clear()
+        lstSyd.Items.Clear()
+
+        'Code to validate whether the selected number is between 10 and 35 and prompt the user to try again if not'
+        If Not IsNumeric(cmbUsefulLife.Text) Then
+            MessageBox.Show("That was not a valid useful life, please try selecting between 3 years and 20 years", "User error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf cmbUsefulLife.Text < 3 Or cmbUsefulLife.Text > 20 Then
+            MessageBox.Show("That was not a valid useful life, please try selecting between 3 years and 20 years", "User error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            'Assignment of information to variables passed into text boxes'
+            usefulLife = cmbUsefulLife.Text
+        End If
+
+        'Code to validate whether cost entry was numeric or not and prompt user to try again'
+        If Not IsNumeric(txtCost.Text) Then
+            MessageBox.Show("That was not a valid entry of cost, please try again", "User error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            'Assignment of information to variables passed into text boxes'
+            cost = txtCost.Text
+        End If
+
+        'Code to validate whether cost entry was numeric or not and prompt user to try again'
+        If Not IsNumeric(txtSalvage.Text) Then
+            MessageBox.Show("That was not a valid entry of what the salvage value might be, please try again", "User error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            'Assignment of information to variables passed into text boxes'
+            salvageVal = txtSalvage.Text
+        End If
+
+        'placing first line of information into lstDdb and lstSyd respectivley'
+        lstDdb.Items.Add("Year               Depreciation")
+        lstSyd.Items.Add("Year               Depreciation")
+
+        'iterating through a range of numbers and calculating desired output and printing it into the respective list boxes'
+        For i As Double = 1 To usefulLife
+            lstDdb.Items.Add(i & "                    " & String.Format("{0:0.00}", Math.Round(Financial.DDB(cost, salvageVal, usefulLife, i), 3)))
+        Next
+        For j As Double = 1 To usefulLife
+            lstSyd.Items.Add(j & "                    " & String.Format("{0:0.00}", Math.Round(Financial.SYD(cost, salvageVal, usefulLife, j), 3)))
+        Next
+
+    End Sub
+
+    Private Sub txtCost_TextChanged(sender As Object, e As EventArgs) Handles txtCost.TextChanged
+
+        'code to allow enter button to be used as a click event for btnDisplay'
+        AcceptButton = btnDisplay
+
+    End Sub
+
+    Private Sub txtSalvage_TextChanged(sender As Object, e As EventArgs) Handles txtSalvage.TextChanged
+
+        'code to allow enter button to be used as a click event for btnDisplay'
+        AcceptButton = btnDisplay
+
+    End Sub
+
+    Private Sub cmbUsefulLife_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbUsefulLife.SelectedIndexChanged
+
+        'code to allow enter button to be used as a click event for btnDisplay'
+        AcceptButton = btnDisplay
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        'code to close the application'
+        Me.Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
